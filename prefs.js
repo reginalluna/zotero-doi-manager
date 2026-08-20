b31a57daf73ff632656f6ad6aa2d22d63421b264
Xenon von Verdict <54960230+reginalluna@users.noreply.github.com>
2026-08-20 16:29:16 -0400

    Enable monthly DOI verification by default

--- prefs.js
+++ prefs.js
@@ -1,4 +1,6 @@
 pref("extensions.shortdoi.autoretrieve", "long");
+pref("extensions.shortdoi.monthly_verify", true);
+pref("extensions.shortdoi.monthly_verify_last_run", "");
 pref("extensions.shortdoi.tag_invalid", "\u26A0\uFE0F Invalid DOI");
 pref("extensions.shortdoi.tag_multiple", "\u2753 Multiple DOI");
 pref("extensions.shortdoi.tag_nodoi", "\u26D4 No DOI found");
